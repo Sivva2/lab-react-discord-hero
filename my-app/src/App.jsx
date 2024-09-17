c9365c19775a39f4c4edcf308413511a28c66820
Kevin Abaskaran <shiva94460@gmail.com>
2024-09-17 16:29:49 +0200

    not solved lab

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,8 +1,17 @@
 import "./App.css";
-function App() {
-  return <div className="App"></div>;
-}
+import logo from "./assets/discord-logo-white.png";
+import menu from "./assets/menu-icon.png";
+import background from "./assets/discord-background.png";
 
-return <img src={image} alt="background" />;
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <img src={logo} alt="discord-logo" className="logo" />
+        <img src={menu} alt="menu" className="menu" />
+      </nav>
+    </div>
+  );
+}
 
 export default App;
